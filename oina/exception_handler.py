c1f50b250265903f6b04209051660367580ba19f
bakdauletbolat <bakkosh21345@gmail.com>
2025-01-29 01:31:52 +0430

    feat: exclude user from filter

--- oina/exception_handler.py
+++ oina/exception_handler.py
@@ -1,16 +1,21 @@
 import sys
 import traceback
 
+from loguru import logger
 from rest_framework import status
 from rest_framework.exceptions import APIException
 from rest_framework.response import Response
 from rest_framework.views import exception_handler
+
+logger.remove()
+logger.add(sys.stdout, colorize=True, format="<green>{time}</green> <level>{message}</level> - {extra}")
 
 
 def custom_exception_handler(exc, context):
     response = exception_handler(exc, context)
 
     if response is not None and isinstance(exc, APIException):
+        logger.bind(error_response=str(response.data)).error("Ошибка from handler")
         return response
 
     exc_type, exc_value, exc_tb = sys.exc_info()
@@ -26,5 +31,8 @@ def custom_exception_handler(exc, context):
             "traceback": tb_summary
         },
     }
+
+
+    logger.bind(error_response=custom_response).error("Ошибка from handler")
 
     return Response(custom_response, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
