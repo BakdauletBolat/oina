46814f3c44ef18b55c395ecab310ff579d1bd071
BakdauletBolat <61851041+BakdauletBolat@users.noreply.github.com>
2025-01-29 17:59:38 +0500

    fix

--- oina/urls.py
+++ oina/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from django.contrib import admin
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
